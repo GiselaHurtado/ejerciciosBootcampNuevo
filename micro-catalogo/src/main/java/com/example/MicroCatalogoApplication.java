fa29aecf739cf63b06537f4c5e96e733ca3b5756
GiselaHurtado <hurtadogiselasoledad@gmail.com>
2025-03-13 16:57:18 +0100

    configuration of main app done

--- micro-catalogo/src/main/java/com/example/MicroCatalogoApplication.java
+++ micro-catalogo/src/main/java/com/example/MicroCatalogoApplication.java
@@ -1,13 +1,34 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.domains.contracts.repositories.ActoresRepository;
+import com.example.domains.entities.Actor;
+
 @SpringBootApplication
-public class MicroCatalogoApplication {
+public class MicroCatalogoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroCatalogoApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.err.println("Aplicacion arrancada");
+		ejemplosDatos();
+	}
+	
+	@Autowired
+	private ActoresRepository dao;
+	
+	private void ejemplosDatos() {
+		var actor = new Actor(0, "Peoito", "Grillo");
+		//var item = dao.findById(204);
+		dao.save(actor);
+		dao.findAll().forEach(System.err::println);
 	}
 
 }
