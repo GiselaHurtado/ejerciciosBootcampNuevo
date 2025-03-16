e51adb71a99583c89f3d451799a32563ba040c80
GiselaHurtado <hurtadogiselasoledad@gmail.com>
2025-03-16 20:58:38 +0100

    actor short fixed

--- micro-catalogo/src/main/java/com/example/domains/entities/dtos/ActorShort.java
+++ micro-catalogo/src/main/java/com/example/domains/entities/dtos/ActorShort.java
@@ -1,8 +1,11 @@
 package com.example.domains.entities.dtos;
 
+
+import org.springframework.beans.factory.annotation.Value;
+
 public interface ActorShort {
-	int getActorId();
-	@Value("#{target")
+	@Value("#{target.actorId}")
+	int getId();
+	@Value("#{target.lastName + ', ' + target.firstName}")
 	String getNombre();
-	
 }
