13fa52455b340e9dcdd922fd27c6be0646a6aba1
GiselaHurtado <hurtadogiselasoledad@gmail.com>
2025-03-16 20:55:10 +0100

    Actores repository

--- micro-catalogo/src/main/java/com/example/domains/contracts/repositories/ActoresRepository.java
+++ micro-catalogo/src/main/java/com/example/domains/contracts/repositories/ActoresRepository.java
@@ -1,8 +1,7 @@
 package com.example.domains.contracts.repositories;
-
+package com.example.domains.contracts.repositories;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,18 +13,18 @@ import com.example.domains.entities.dtos.ActorDTO;
 import com.example.domains.entities.dtos.ActorShort;
 
 public interface ActoresRepository extends JpaRepository<Actor, Integer>, JpaSpecificationExecutor<Actor> {
-    List<Actor> findTop5ByFirstNameStartingWithOrderByLastNameDesc(String prefijo);
-    List<Actor> findTop5ByFirstNameStartingWith(String prefijo, Sort orderBy);
-    List<Actor> findByActorIdGreaterThan(int id);
+	List<Actor> findTop5ByFirstNameStartingWithOrderByLastNameDesc(String prefijo);
+	List<Actor> findTop5ByFirstNameStartingWith(String prefijo, Sort orderBy);
+	
+	List<Actor> findByActorIdGreaterThan(int id);
+	@Query(value = "SELECT a FROM Actor a WHERE a.actorId > ?1")
+	List<Actor> findNovedadesJPQL(int id);
+	@Query(value = "SELECT * FROM actor a WHERE a.actor_id > :id", nativeQuery = true)
+	List<Actor> findNovedadesSQL(int id);
+	
+	List<ActorDTO> queryByActorIdGreaterThan(int id);
+	List<ActorShort> getByActorIdGreaterThan(int id);
 
-    @Query("SELECT a FROM Actor a WHERE a.actorId > ?1")
-    List<Actor> findNovedadesJPQL(int id);
-
-    @Query(value = "SELECT * FROM actor WHERE actor_id > :id", nativeQuery = true)
-    List<Actor> findNovedadesSQL(int id);
-    
-    List<ActorDTO> queryByActorIdGreaterThan1(int id);
-    List<ActorShort> queryByActorIdGreaterThan(int id);
-
+	<T> List<T> findByActorIdGreaterThan(int id, Class<T> type);
 
 }
--- micro-catalogo/src/main/java/com/example/domains/entities/dtos/ActorShort.java
+++ micro-catalogo/src/main/java/com/example/domains/entities/dtos/ActorShort.java
@@ -2,6 +2,7 @@ package com.example.domains.entities.dtos;
 
 public interface ActorShort {
 	int getActorId();
+	@Value("#{target")
 	String getNombre();
 	
 }
