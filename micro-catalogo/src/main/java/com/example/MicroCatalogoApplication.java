4cd301aa88bba76cdfb6765d74e4f0c0190db0c0
GiselaHurtado <hurtadogiselasoledad@gmail.com>
2025-03-16 21:17:17 +0100

    proyecciones

--- micro-catalogo/src/main/java/com/example/MicroCatalogoApplication.java
+++ micro-catalogo/src/main/java/com/example/MicroCatalogoApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.domains.contracts.repositories.ActoresRepository;
-import com.example.domains.entities.Actor;
+import com.example.domains.contracts.services.ActoresService;
+import com.example.domains.entities.dtos.ActorDTO;
+import com.example.domains.entities.dtos.ActorShort;
+
+import jakarta.transaction.Transactional;
 
 @SpringBootApplication
 public class MicroCatalogoApplication implements CommandLineRunner {
@@ -16,6 +20,7 @@ public class MicroCatalogoApplication implements CommandLineRunner {
 	}
 	
 	@Override
+	@Transactional
 	public void run(String... args) throws Exception {
 		System.err.println("Aplicacion arrancada");
 		ejemplosDatos();
@@ -24,12 +29,27 @@ public class MicroCatalogoApplication implements CommandLineRunner {
 	@Autowired
 	private ActoresRepository dao;
 	
+	@Autowired
+	private ActoresService srv;
+	
 	private void ejemplosDatos() {
-		var actor = new Actor(0, null, "IAN MIKU");
+		//var actor = new Actor(0, null, "IAN MIKU");
 		//if(actor.isValid());
-		dao.save(actor);
+		//dao.save(actor);
 		//else {
-		System.err.println(actor.getErrorsMessage());
+		dao.findByActorIdGreaterThan(200).forEach(System.err::println);
+		dao.findByActorIdGreaterThan(200, ActorDTO.class).forEach(System.err::println);
+		dao.findByActorIdGreaterThan(200, ActorShort.class).forEach(o -> System.err.println(o.getId() + " " + o.getNombre()));
+	}
+		//System.err.println(actor.getErrorsMessage());
+
+	public ActoresService getSrv() {
+		return srv;
+	}
+
+	public void setSrv(ActoresService srv) {
+		this.srv = srv;
 	}
 	}
+	
 
