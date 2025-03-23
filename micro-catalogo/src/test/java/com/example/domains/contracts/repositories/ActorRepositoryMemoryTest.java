7c603d3860e3eb6e3a06bdf84f7d3eebfa932b89
GiselaHurtado <hurtadogiselasoledad@gmail.com>
2025-03-23 18:19:48 +0100

    actorRepositoryReal TEst and memoryTest fixed

--- micro-catalogo/src/test/java/com/example/domains/contracts/repositories/ActorRepositoryMemoryTest.java
+++ micro-catalogo/src/test/java/com/example/domains/contracts/repositories/ActorRepositoryMemoryTest.java
@@ -1,102 +1,59 @@
-//package com.example.domains.contracts.repositories;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//import java.sql.Timestamp;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-//import org.springframework.data.domain.Sort;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//import com.example.domains.entities.Actor;
-//import com.example.test.utils.Lentos;
-//
-//@DataJpaTest
-//@ActiveProfiles("test")
-//@Lentos
-//class ActorRepositoryMemoryTest {
-//    @Autowired
-//    private TestEntityManager em;
-//    
-//    @Autowired
-//    ActoresRepository dao;
-//    
-//    @BeforeEach
-//    void setUp() throws Exception {
-//        em.getEntityManager().createQuery("DELETE FROM Actor").executeUpdate();
-//        
-//        var item = new Actor(0, "Pepito", "GRILLO");
-//        item.setLastUpdate(Timestamp.valueOf("2019-01-01 00:00:00"));
-//        em.persist(item);
-//        item = new Actor(0, "Carmelo", "COTON");
-//        item.setLastUpdate(Timestamp.valueOf("2019-01-01 00:00:00"));
-//        em.persist(item);
-//        item = new Actor(0, "Capitan", "TAN");
-//        item.setLastUpdate(Timestamp.valueOf("2019-01-01 00:00:00"));
-//        em.persist(item);
-//        
-//        em.flush();
-//    }
-//
+package com.example.domains.contracts.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.sql.Timestamp;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import com.example.domains.entities.Actor;
+
+@DataJpaTest
+@ActiveProfiles("test")  
+class ActorRepositoryMemoryTest {
+
+    @Autowired
+    private org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager em;
+
+    @Autowired
+    private ActoresRepository dao;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        var item = new Actor(0, "Pepito", "GRILLO");
+        item.setLastUpdate(Timestamp.valueOf("2019-01-01 00:00:00"));
+        em.persist(item);
+        item = new Actor(0, "Carmelo", "COTON");
+        item.setLastUpdate(Timestamp.valueOf("2019-01-01 00:00:00"));
+        em.persist(item);
+        item = new Actor(0, "Capitan", "TAN");
+        item.setLastUpdate(Timestamp.valueOf("2019-01-01 00:00:00"));
+        em.persist(item);
+      
+        em.flush();
+    }
+
+    @Test
+    void testGetAll_isNotEmpty() {
+        var rslt = dao.findAll();
+        assertThat(rslt.size()).isEqualTo(3);
+       
+        //assertThat(dao.findTop5ByFirstNameStartingWithOrderByLastNameDesc("C").size()).isEqualTo(1);
+        //assertThat(dao.findNovedadesJPQL(1).size()).isEqualTo(3);
+    }
+
+    @Test
+    void testGetOne() {
+        var firstActorId = dao.findAll().get(0).getActorId();
+        var item = dao.findById(firstActorId);
+        assertThat(item.isPresent()).isTrue();
+        assertEquals("Pepito", item.get().getFirstName());
+    }
+    
 //    @Test
-//    void testGetAll_isNotEmpty() {
-//        var rslt = dao.findAll();
-//        assertThat(rslt.size()).isEqualTo(3);
+//    void findBySQLTest() {
+//        assertThat(dao.findNovedadesSQL(1).size()).isGreaterThan(0);
 //    }
-//    
-//    @Test
-//    void testFindTop5ByFirstNameStartingWithOrderByLastNameDesc() {
-//        var rslt = dao.findTop5ByFirstNameStartingWithOrderByLastNameDesc("C");
-//        assertThat(rslt.size()).isEqualTo(2);
-//    }
-//    
-//    @Test
-//    void testFindTop5ByFirstNameStartingWith() {
-//        var rslt = dao.findTop5ByFirstNameStartingWith("C", Sort.by("lastName").descending());
-//        assertThat(rslt.size()).isEqualTo(2);
-//    }
-//    
-//    @Test
-//    void testFindByActorIdGreaterThan() {
-//        var rslt = dao.findByActorIdGreaterThan(0);
-//        assertThat(rslt.size()).isEqualTo(3);
-//    }
-//    
-//    @Test 
-//    void testFindNovedadesJPQL() {
-//        var rslt = dao.findNovedadesJPQL(0);
-//        assertThat(rslt.size()).isEqualTo(3);
-//    }
-//    
-//    @Test
-//    void testGetOne() {
-//        var actores = dao.findAll();
-//        if (!actores.isEmpty()) {
-//            var item = dao.findById(actores.get(0).getActorId());
-//            assertThat(item.isPresent()).isTrue();
-//        }
-//    }
-//    
-//    @Test
-//    void testFindBySQLTest() {
-//        var rslt = dao.findNovedadesSQL(0);
-//        assertThat(rslt.size()).isGreaterThan(0);
-//    }
-//    
-//    @Test
-//    void testQueryByActorIdGreaterThan() {
-//        var rslt = dao.queryByActorIdGreaterThan(0);
-//        assertThat(rslt.size()).isEqualTo(3);
-//    }
-//    
-//    @Test
-//    void testGetByActorIdGreaterThan() {
-//        var rslt = dao.getByActorIdGreaterThan(0);
-//        assertThat(rslt.size()).isEqualTo(3);
-//    }
-//}
+}
