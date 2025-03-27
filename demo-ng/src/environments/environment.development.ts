dce6e985d042913a42a6eee0953a37eb067ca501
GiselaHurtado <hurtadogiselasoledad@gmail.com>
2025-03-27 11:54:31 +0100

    enviroment done

---
+++ demo-ng/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+import { ɵXSS_SECURITY_URL } from "@angular/core";
+
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3000',    
+    securityUrl: 'http://localhost:3000',
+};
