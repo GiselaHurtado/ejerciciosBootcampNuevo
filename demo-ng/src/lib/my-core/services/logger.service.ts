4b3f92cf9f11b34a1761bebc52d05e50b678b73b
GiselaHurtado <hurtadogiselasoledad@gmail.com>
2025-03-28 11:55:06 +0100

    different message levels

--- demo-ng/src/lib/my-core/services/logger.service.ts
+++ demo-ng/src/lib/my-core/services/logger.service.ts
@@ -4,25 +4,32 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LoggerService {
+  private nivel: number = Number.MAX_VALUE;
+
 
   constructor() { }
 
   public error(message: string) : void {
+    if(this.nivel > 0)
     console.error(message);
   }
 
   public warn(message: string) : void {
+    if(this.nivel > 1)
     console.warn(message);
   }
 
   public info(message: string) : void {
+    if(this.nivel > 2){
     if(console.info)
     console.info(message);
     else
     console.log(message);
   }
+}
 
   public log(message: string) : void {
+    if(this.nivel > 3)
     console.log(message);
   }
 }
